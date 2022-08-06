705162649bc68ae609c4d291fb00971b1cc2550e
linek150 <linek11@vp.pl>
2022-08-06 15:15:24 +0200

    Tuple of LR to test for every run algorithm

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ if DETERMINISTIC_RES:
 
 dataset_ = CustomDataset(DATASET_NAME, n_dim=N_DIM)
 
+
 bs=BATCH_SIZE if BATCH_SIZE is not None else len(dataset_)
 valid_dataloader = None
 if VALIDATION:
@@ -27,23 +28,25 @@ dataloader_ = torch.utils.data.DataLoader(dataset_, batch_size=bs)
 no_inputs = dataset_.input_size
 no_outputs = dataset_.output_size
 loss = dataset_.default_loss if LOSS == LossEnum.DEFAULT else LOSS
-
-if SCINOL_ONLY:
-    optimizers = []  # [Scinol2Dl]
+if RUN_OTHER_ALGORITHMS:
+    optimizers = [Adam, SGD] # [Scinol2Dl]
 else:
-    optimizers = [Adam, COCOB_Backprop]
-
+    optimizers = []
+writer = None
 for run_no in range(NO_RUNS):
     model = get_model(MODEL_TYPE, no_inputs, no_outputs, HIDDEN_LAYERS, ACTIVATION.value)
+
     for optimizer in optimizers:
+        for lr in LR:
+            clean_model = copy.deepcopy(model).to(DEVICE)
+            writer = SummaryWriter(log_dir=f'{WRITER_PREFIX}{optimizer.__name__}/{lr}/{str(run_no)}')
+            train(dataloader_, clean_model, writer, optim=optimizer, no_epochs=NO_EPOCHS, log_grads=LOG_GRADS_AND_WEIGHTS,
+                  loss=loss, val_dataloader=valid_dataloader, lr=lr, log_to_pickle=LOSS_TO_PICKLE)
+    if RUN_SCINOL:
         clean_model = copy.deepcopy(model).to(DEVICE)
-        writer = SummaryWriter(log_dir=f'{WRITER_PREFIX}{optimizer.__name__+str(run_no)}')
-        train(dataloader_, clean_model, writer, optim=optimizer, no_epochs=NO_EPOCHS, log_grads=LOG_GRADS_AND_WEIGHTS,
-              loss=loss, val_dataloader = valid_dataloader)
-    clean_model = copy.deepcopy(model).to(DEVICE)
-    #scinol_model = get_scinol_model(MODEL_TYPE, no_inputs, no_outputs, HIDDEN_LAYERS, ACTIVATION.value, ETA_INIT).to(DEVICE)
-    scinol_model = adapt_to_scinol(clean_model)
-    writer = SummaryWriter(log_dir=f'{WRITER_PREFIX}{SCINOL_PREFIX}Scinol{str(run_no)}')
-    train(dataloader_, scinol_model, writer, no_epochs=NO_EPOCHS, loss=loss, log_grads=LOG_GRADS_AND_WEIGHTS,
-          log_scinol_params_=LOG_SCINOL_PARAMS, val_dataloader=valid_dataloader)
+        #scinol_model = get_scinol_model(MODEL_TYPE, no_inputs, no_outputs, HIDDEN_LAYERS, ACTIVATION.value, ETA_INIT).to(DEVICE)
+        scinol_model = adapt_to_scinol(clean_model)
+        writer = SummaryWriter(log_dir=f'{WRITER_PREFIX}{SCINOL_PREFIX}Scinol{str(run_no)}')
+        train(dataloader_, scinol_model, writer, no_epochs=NO_EPOCHS, loss=loss, log_grads=LOG_GRADS_AND_WEIGHTS,
+              log_scinol_params_=LOG_SCINOL_PARAMS, val_dataloader=valid_dataloader, log_to_pickle=LOSS_TO_PICKLE)
     writer.flush()
